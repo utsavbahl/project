ce8fe63f0f24a0150f3a920bb334d7e278061721
Your Name <ub@williams.edu>
2022-11-23 13:36:12 -0500

    New Coding

--- Matching Idea.R
+++ Matching Idea.R
@@ -99,9 +99,25 @@ t2 <- merge(x = data, y = t1, by = c("Name", "year"), all.x = TRUE)
 t2$post[is.na(t2$post)] = 0
 data <- t2
 
+#Further Cleaning of Data
+data$Grad_Rate <- as.numeric(data$Grad_Rate)
+
+data$Grad_White <- paste(data$Grad_White, data$Grad_White_2010, sep = "")
+data$Grad_White <- gsub("NA", "", data$Grad_White)
+data$Grad_White <- as.numeric(data$Grad_White)
+
+data$Grad_Black <- paste(data$Grad_Black, data$Grad_Black_2010, sep = "")
+data$Grad_Black <- gsub("NA", "", data$Grad_Black)
+data$Grad_Black <- as.numeric(data$Grad_Black)
+
+data$Grad_Asian <- paste(data$Grad_Asian, data$Grad_Asian_2010, sep = "")
+data$Grad_Asian <- gsub("NA", "", data$Grad_Asian)
+data$Grad_Asian <- as.numeric(data$Grad_Asian)
 
 
 
+data <- read_csv("dataset.csv")
+data <- data[,-1]
     
   
 

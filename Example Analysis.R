e6508998f362821036f6f61a0417d4ed200cdea1
Your Name <ub@williams.edu>
2022-11-30 10:19:42 -0500

    Example

--- Example Analysis.R
+++ Example Analysis.R
@@ -4,10 +4,11 @@ library(did2s)
 #Simple TWFE Analysis with DiD Estimate
 did2s(
   data = data,
-  yname = "Grad_Rate",
+  yname = "Grad_Aian",
   first_stage = ~ 0 | Name + year,
   second_stage = ~ i(post, ref=0),
   treatment = "post",
   cluster_var = "Name"
 )
+
 
